5f0e26539dc06268b72faa404dea893f59a7250f
ViniciusFn <contact.viniciusfn@gmail.com>
2026-05-05 21:22:30 -0300

    🔥 hotfix: date update

--- lang/pt_BR/view/layout.php
+++ lang/pt_BR/view/layout.php
@@ -17,6 +17,6 @@ return [
     'locale_no_ui' => 'sem interface',
     'footer' => [
         'contact' => 'Contato:',
-        'copyright' => '2024 - UTFPR - Unicentro',
+        'copyright' => ':year - UTFPR - Unicentro',
     ],
 ];
